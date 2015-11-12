fe54a63fe177ba4ae67edb5c5d4aa3e53bb48b94
MrMallet <g00304978@gmit.ie>
2015-11-12 13:43:45 +0000

    used urlencoded from vlads code and its passing/storing values

--- diamond.js
+++ diamond.js
@@ -6,9 +6,11 @@ var bodyParser = require('body-parser');
 var app = express();
 // Import the fs module so that we can read in files.
 var fs = require('fs');
-// Read in the text file and parse it for JSON.
-app.use(bodyParser.json());
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // this line from vlads passes the VALUES
+
+// Read in the text file and parse it for JSON.
 var data = JSON.parse(fs.readFileSync('prices-of-diamondsShort.json','utf8'));
 
 var sqlite3 = require('sqlite3').verbose();
@@ -16,19 +18,14 @@ var db = new sqlite3.Database(':memory:');
 
 db.serialize(function() {
   db.run("CREATE TABLE diamond( 'id' INTEGER PRIMARY KEY NOT NULL, 'carat' REAL, 'cut' TEXT, 'color' TEXT, 'clarity' TEXT, 'depth' REAL, 'table' INTEGER, 'price' real, 'x' REAL, 'y' REAL, 'z' REAL)");
-
   var stmt = db.prepare("INSERT INTO diamond VALUES (NULL,?,?,?,?,?,?,?,?,?,?)");
-
   data.forEach(function (fill){
     stmt.run(fill.carat, fill.cut, fill.color, fill.clarity, fill.depth, fill.table, fill.price, fill.x, fill.y, fill.z );
-
   });
   stmt.finalize();
-
 });
 
 //db.close();
-
 app.get('/', function(req, res) {
   res.send("This is the Diamond API.");
 });
@@ -40,26 +37,13 @@ app.get('/diamond/id/:id', function(req, res) {
       //console.log("Made it this far");
     });
 });
+
 app.post('/diamonds/add/', function(req, res){
   var stmt = db.prepare("INSERT INTO diamond VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
   stmt.run(req.body.carat, req.body.cut, req.body.color, req.body.clarity, req.body.depth, req.body.tablefield, req.body.price, req.body.x, req.body.y, req.body.z);
   stmt.finalize();
   res.send("Diamond Added\n");
 });
-
-/*app.post('/diamonds/add/', function(req, res){
-  console.log("Made it this far");
-  var stmt = db.prepare("INSERT INTO diamond VALUES (NULL,?,?,?,?,?,?,?,?,?,?)");
-  stmt.run(req.body.carat, req.body.cut, req.body.color, req.body.clarity, req.body.depth, req.body.table, req.body.price, req.body.x, req.body.y, req.body.z );
-  stmt.finalize();
-  console.log("Diamond Added");
-  res.send("Diamond Added\n");
-
-});
-*/
-
-
-
 
 // Start the server.
 var server = app.listen(8000);
